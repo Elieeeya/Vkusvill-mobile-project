89178be43ed7c43b1b14bce9c2157247d14d1fbb
Илья Сляднев <97525983+Elieeeya@users.noreply.github.com>
2022-11-04 16:26:06 +0300

    Update main_page

--- test/pages/main_page.py
+++ test/pages/main_page.py
@@ -1,3 +1,5 @@
+import time
+
 from appium.webdriver.common.appiumby import AppiumBy
 from selene import have, be
 from test.conftest import *
@@ -6,62 +8,71 @@ from test.conftest import *
 class MainPageVkusvill:
 
     def swipe_list(self):
-        browser.wait.until(30)
-        browser.element('[id="ru.vkusvill:id/btn_enter"]').click()
-        browser.wait.until(30)
-        browser.element('[id="ru.vkusvill:id/bottom_item_profile"]').click()
-        browser.wait.until(2)
-        browser.element('[id="ru.vkusvill:id/bottom_item_catalog"]').click()
-        browser.wait.until(2)
-        browser.element('[id="ru.vkusvill:id/bottom_item_history"]').click()
-        browser.wait.until(2)
-        browser.element('[id="ru.vkusvill:id/bottom_item_support"]').click()
-        browser.wait.until(2)
+        browser.element(
+            (AppiumBy.ID, 'ru.vkusvill:id/btn_enter')).click()
+        browser.element(
+            (AppiumBy.ID, 'ru.vkusvill:id/bottom_item_profile')).click()
+        browser.element(
+            (AppiumBy.ID, 'ru.vkusvill:id/bottom_item_catalog')).click()
+        browser.element(
+            (AppiumBy.ID, 'ru.vkusvill:id/bottom_item_history')).click()
+        browser.element(
+            (AppiumBy.ID, 'ru.vkusvill:id/bottom_item_support')).click()
         browser.quit()
 
     def loacation(self):
-        browser.wait.until(10)
-        browser.element('[id="ru.vkusvill:id/btn_enter"]').click()
-        browser.element('[id="ru.vkusvill:id/bottom_item_catalog"]').click()
-        browser.element('[id="ru.vkusvill:id/button_service_not"]').click()
-        browser.element('[id="com.android.permissioncontroller:id/permission_allow_foreground_only_button"]').click()
-        browser.element('[id="ru.vkusvill:id/tv_address"]').click()
-        browser.wait.until(2)
-        browser.element('[id="ru.vkusvill:id/search"]').send_keys('Moscow, Prospekt Mira, 95с1')
-        browser.wait.until(2)
-        browser.element('[id="ru.vkusvill:id/rl_root"]').click()
-        browser.wait.until(2)
-        browser.element('[id="ru.vkusvill:id/btn_select"]').click()
-        browser.wait.until(2)
-        browser.element('[id="ru.vkusvill:id/button_rules_ok"]').click()
+        browser.element(
+            (AppiumBy.ID, 'ru.vkusvill:id/btn_enter')).click()
+        browser.element(
+            (AppiumBy.ID, 'ru.vkusvill:id/bottom_item_catalog')).click()
+        browser.element(
+            (AppiumBy.ID, 'ru.vkusvill:id/button_service_not')).click()
+        browser.element(
+            (AppiumBy.ID, 'com.android.permissioncontroller:id/permission_allow_foreground_only_button')).click()
+        browser.element(
+            (AppiumBy.ID, 'ru.vkusvill:id/tv_address')).click()
+        browser.element(
+            (AppiumBy.ID, 'ru.vkusvill:id/search')).send_keys('Moscow, Prospekt Mira, 95с1')
+        browser.element(
+            (AppiumBy.ID, 'ru.vkusvill:id/rl_root')).click()
+        browser.element(
+            (AppiumBy.ID, 'ru.vkusvill:id/btn_select')).click()
+        browser.element(
+            (AppiumBy.ID, 'ru.vkusvill:id/button_rules_ok')).click()
         browser.quit()
 
 
 
     def search(self):
-        browser.wait.until(10)
-        browser.element('[id="ru.vkusvill:id/btn_enter"]').click()
-        browser.element('[id="ru.vkusvill:id/bottom_item_catalog"]').click()
-        browser.element('[id="ru.vkusvill:id/ll_search"]').click()
-        browser.element('[id="ru.vkusvill:id/et_search"]').send_keys('Milka')
-        browser.wait.until(1)
-        browser.element('[id="ru.vkusvill:id/ll_cart_empty"]').click()
+        browser.element(
+            (AppiumBy.ID, 'ru.vkusvill:id/btn_enter')).click()
+        browser.element(
+            (AppiumBy.ID, 'ru.vkusvill:id/bottom_item_catalog')).click()
+        browser.element(
+            (AppiumBy.ID, 'ru.vkusvill:id/ll_search')).click()
+        browser.element(
+            (AppiumBy.ID, 'ru.vkusvill:id/et_search')).send_keys('Milka')
+        browser.element(
+            (AppiumBy.ID, 'ru.vkusvill:id/ll_cart_empty')).click()
         browser.quit()
 
     def contacts(self):
-        browser.wait.until(10)
-        browser.element('[id="ru.vkusvill:id/btn_enter"]').click()
-        browser.element('[id="ru.vkusvill:id/bottom_item_support"]').click()
-        browser.element('[id="ru.vkusvill:id/tv_contact_call"]').click()
-        browser.wait.until(1)
+        browser.element(
+            (AppiumBy.ID, 'ru.vkusvill:id/btn_enter')).click()
+        browser.element(
+            (AppiumBy.ID, 'ru.vkusvill:id/bottom_item_support')).click()
+        browser.element(
+            (AppiumBy.ID, 'ru.vkusvill:id/tv_contact_call')).click()
         browser.quit()
 
     def loyalty_program(self):
-        browser.wait.until(10)
-        browser.element('[id="ru.vkusvill:id/btn_enter"]').click()
-        browser.element('[id="ru.vkusvill:id/bottom_item_support"]').click()
-        browser.element('[id="ru.vkusvill:id/sl"]').click()
-        browser.wait.until(1)
+        browser.element(
+            (AppiumBy.ID, 'ru.vkusvill:id/btn_enter')).click()
+        browser.element(
+            (AppiumBy.ID, 'ru.vkusvill:id/bottom_item_support')).click()
+        browser.element(
+            (AppiumBy.ID, 'ru.vkusvill:id/sl')).click()
+        time.sleep(2)
         browser.quit()
 
 
